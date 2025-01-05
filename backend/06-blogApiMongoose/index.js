b8ed1de8edb5190fb7caa0430dc303c057fcdd8e
okan87 <okankaplan1907@gmail.com>
2025-01-05 20:36:25 +0100

    structure is created

---
+++ backend/06-blogApiMongoose/index.js
@@ -0,0 +1,27 @@
+"use strict";
+/* ---------------------------------------------------------
+*    EXPRESSJS - BLOG Project with Mongoose
+---------------------------------------------------------- */
+
+/*
+    * $ npm init -y
+    * $ npm i express dotenv express-async-errors mongoose
+*/
+
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 8000;
+
+/* ------------------------------------------------------- */
+// //? Accept json data & convert to object:
+// app.use(express.json());
+
+// //? Router:
+// app.use(require("./app/routes/todo"));
+
+// //? errorHandler (catcherrors):
+// app.use(require("./app/errorHandler"));
+
+/* ------------------------------------------------------- */
+app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
