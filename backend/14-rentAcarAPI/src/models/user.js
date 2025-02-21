031424e5458d4963bfb74f65ba2448e55f5eb590
okan87 <okankaplan1907@gmail.com>
2025-02-21 22:11:35 +0100

    user-model

--- backend/14-rentAcarAPI/src/models/user.js
+++ backend/14-rentAcarAPI/src/models/user.js
@@ -1,8 +1,8 @@
-"use strict"
+"use strict";
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require("../configs/dbConnection");
 /* ------------------------------------------------------- *
 {
     "username": "admin",
@@ -24,53 +24,55 @@ const { mongoose } = require('../configs/dbConnection')
 }
 /* ------------------------------------------------------- */
 // User Model:
-
-const passwordEncrypt = require('../helpers/passwordEncrypt')
-
-const UserSchema = new mongoose.Schema({
+const passwordEncrypt = require("../helpers/passwordEncrypt");
+const UserSchema = new mongoose.Schema(
+  {
     username: {
-        type: String,
-        trim: true,
-        required: true,
-        unique: true
+      type: String,
+      trim: true,
+      required: true,
+      unique: true,
     },
-
     password: {
-        type: String,
-        trim: true,
-        required: true,
-        // select: false,
-        set: (password) => passwordEncrypt(password),
+      type: String,
+      trim: true,
+      required: true,
+      // select: false,
+      set: (password) => passwordEncrypt(password),
     },
-
     email: {
-        type: String,
-        trim: true,
-        required: [true, 'Email field must be required.'],
-        unique: [true, 'There is this email. Email field must be unique.'],
-        validate: [
-            (email) =>  {
-                const emailRegexCheck = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-                return emailRegexCheck.test(email)
-            },
-            'Email type is not correct.'
-        ]
+      type: String,
+      trim: true,
+      required: [true, "Email field must be required."],
+      unique: [true, "There is this email. Email field must be unique."],
+      validate: [
+        (email) => {
+          const emailRegexCheck =
+            /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+          return emailRegexCheck.test(email);
+        },
+        "Email type is not correct.",
+      ],
     },
-
+    firstName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    lasttName: {
+      type: String,
+      trim: true,
+      required: true,
+    },
     isActive: {
-        type: Boolean,
-        default: true
+      type: Boolean,
+      default: true,
     },
-
-    isStaff: {
-        type: Boolean,
-        default: false
-    },
-
     isAdmin: {
-        type: Boolean,
-        default: false
+      type: Boolean,
+      default: false,
     },
-
-}, { collection: 'users', timestamps: true })
-module.exports = mongoose.model('User', UserSchema)
+  },
+  { collection: "users", timestamps: true }
+);
+module.exports = mongoose.model("User", UserSchema);
