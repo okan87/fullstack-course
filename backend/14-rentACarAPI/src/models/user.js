021d3330cf51caf45475200c5ed10dc1af9f5b45
okan87 <okankaplan1907@gmail.com>
2025-02-27 22:22:50 +0100

    user mvc

--- backend/14-rentACarAPI/src/models/user.js
+++ backend/14-rentACarAPI/src/models/user.js
@@ -2,12 +2,14 @@
 /* -------------------------------------------------------
     NODEJS EXPRESS | CLARUSWAY FullStack Team
 ------------------------------------------------------- */
-const { mongoose } = require('../configs/dbConnection')
+const { mongoose } = require('../configs/dbConnection');
+const passwordEncrypt = require('../helpers/passwordEncrypt');
+
 /* ------------------------------------------------------- *
 {
     "username": "admin",
-    "password": "1234",
-    "email": "admin@site.com",
+    "password": "Test*123456789",
+    "email": "admin@test.com",
     "firstName": "admin",
     "lastName": "admin",
     "isActive": true,
@@ -15,8 +17,8 @@ const { mongoose } = require('../configs/dbConnection')
 }
 {
     "username": "test",
-    "password": "1234",
-    "email": "test@site.com",
+    "password": "Test*123456789",
+    "email": "test@test.com",
     "firstName": "test",
     "lastName": "test",
     "isActive": true,
@@ -24,3 +26,84 @@ const { mongoose } = require('../configs/dbConnection')
 }
 /* ------------------------------------------------------- */
 // User Model:
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'Email field must be required.'],
+        unique: [true, 'There is this email. Email field must be unique.'],
+        validate: [
+            (email) => {
+                const emailRegexCheck = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+                return emailRegexCheck.test(email);
+            },
+            'Email type is not correct.'
+        ]
+    },
+    firstName: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+    isActive: {
+        type: Boolean,
+        default: true,
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+}, { collection: 'users', timestamps: true });
+
+/* ------------------------------------------------------- */
+// Hash the password before saving the user
+UserSchema.pre('save', async function (next) {
+    if (this.isModified('password') || this.isNew) {
+        try {
+            this.password = await passwordEncrypt(this.password);
+            next();
+        } catch (err) {
+            next(err);
+        }
+    } else {
+        next();
+    }
+});
+
+/* ------------------------------------------------------- */
+// Check if email is unique before saving the user
+UserSchema.pre('save', async function (next) {
+    if (this.isModified('email') || this.isNew) {
+        try {
+            const existingUser = await mongoose.models.User.findOne({ email: this.email });
+            if (existingUser) {
+                const error = new Error('Email already exists');
+                error.status = 400;
+                return next(error);
+            }
+        } catch (err) {
+            return next(err);
+        }
+    }
+    next();
+});
+
+/* ------------------------------------------------------- */
+module.exports = mongoose.model('User', UserSchema);
--- backend/14-rentACarAPI/src/routes/user.js
+++ backend/14-rentACarAPI/src/routes/user.js
@@ -5,3 +5,22 @@
 const router = require('express').Router()
 /* ------------------------------------------------------- */
 // routes/user:
+
+const User = require('../controllers/user')
+
+// URL: /users
+
+
+
+router.route('/')
+    .get(User.list)
+    .post(User.create)
+
+router.route('/:id')
+    .get(User.read)
+    .put(User.update)
+    .patch(User.update)
+    .delete(User.delete)
+
+/* ------------------------------------------------------- */
+module.exports = router
